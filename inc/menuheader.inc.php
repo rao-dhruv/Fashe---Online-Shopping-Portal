63d9a8966d8b3eac06cbc7b2a9c21f2dcc289031
Dhruv Rao <57631631+rao-dhruv@users.noreply.github.com>
2024-08-07 15:21:24 -0400

    Add files via upload

---
+++ inc/menuheader.inc.php
@@ -0,0 +1,16 @@
+<div class="categolis">
+		<table>
+			<tr>
+				<th>
+					<a href="saree.php" style="text-decoration: none;color: #fff;padding: 4px 12px;background-color: #24bfae;border-radius: 12px;">Sarees</a>
+				</th>
+				<th><a href="saree.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #c7587e;border-radius: 12px;">Ornaments</a></th>
+				<th><a href="saree.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #c7587e;border-radius: 12px;">Watches</a></th>
+				<th><a href="saree.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #c7587e;border-radius: 12px;">Perfumes</a></th>
+				<th><a href="saree.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #c7587e;border-radius: 12px;">Blazers</a></th>
+				<th><a href="saree.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #c7587e;border-radius: 12px;">T-Shirts</a></th>
+				<th><a href="saree.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #c7587e;border-radius: 12px;">FootWears</a></th>
+				<th><a href="saree.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #c7587e;border-radius: 12px;">Kids-section</a></th>
+			</tr>
+		</table>
+	</div>
